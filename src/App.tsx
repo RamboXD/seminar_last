c709fd281736aa637eff9d65b68b1b40ea56c741
RamboXD <rnazymxan@gmail.com>
2024-04-03 19:32:07 +0500

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { Header } from './components/Header';
+import axios from 'axios';
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 function App() {
   const [count, setCount] = useState<number>(0);
+  const [data, setData] = useState<Todo[] | null>();
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos').then((response) => {
+      setData(response.data);
+    });
+  }, []);
 
   return (
     <>
@@ -26,6 +40,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+
+        {data && (
+          <div>
+            <p>Todo Title: {data[1].title}</p>
+            <p>Completed: {data[1].completed ? 'Yes' : 'No'}</p>
+          </div>
+        )}
       </div>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
